0e41ac6640cc7dfe13887daa1725cabf87bf22a9
orkunmertgs98 <orkunmertgs41@gmail.com>
2020-09-25 01:46:58 +0300

    s

--- src/components/Info/InfoCard.js
+++ src/components/Info/InfoCard.js
@@ -3,11 +3,12 @@ import React from "react";
 
 import "./InfoCard.style.css";
 
-function Info({title,svg,color,p}) {
+function Info({title,Svg,color,p}) {
+  console.log(Svg)
   return (
     <div className="infocard containerCenter">
       <div className={`${color?"logoContainerColor box center-xs shadow":"logoContainer box center-xs shadow"}`}> 
-     <img  src={svg}></img>
+   {Svg}
 
       </div>
       <h2 className="font-1 center-xs margin-top-1 h2-responsive">{title}</h2>
--- src/layout/InfoLayout.js
+++ src/layout/InfoLayout.js
@@ -1,18 +1,76 @@
 import InfoCard from "../components/Info/InfoCard";
-import React from "react"
+import React,{useEffect,ReactDOM,useState} from "react"
 
-import search from "../assets/svg/search.svg";
 
-import photoIcon from "../assets/svg/imageplacer.svg";
-import carIcon from "../assets/svg/car.svg";
-import paymentIcon from "../assets/svg/card.svg";
-import keywordIcon from "../assets/svg/key.svg";
+import Car from "../components/Info/car";
+import Image from "../components/Info/image";
+import Card from "../components/Info/payment";
+import Key from "../components/Info/key";
+import Search from "../components/Info/search";
+
 import "../components/Info/InfoCard.style.css";
 import Title from "../components/titleComponent"
 import Wave from "../components/Info/wave"
-function Info() {
+function Info(props) {
+
+const [enable,set_enable]=useState(false)
+
+const [level,set_level]=useState(0)
+
+
+  let scrollEl = React.createRef();
+ const  handleScroll = e => {
+
+    let element = scrollEl.current
+   if(!enable&&element){
+    if (window.pageYOffset>element.scrollHeight ) {
+      // do something at end of scroll
+      console.log(" 1 kez")
+      set_enable(true)
+    }
+
+   }
+  
+  }
+
+  useEffect(() => {
+
+   
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  });
+
+const increaseLevel=(l)=>{
+
+setTimeout(() => {
+  set_level(l)
+  if(l<5)
+  increaseLevel(l+1)
+}, 1000);
+
+}
+
+
+
+  useEffect(() => {
+if(enable){
+
+set_level(1)
+increaseLevel(2)
+
+
+
+}
+   
+
+  },[enable]);
+
+
   return (
-    <div className="containerCenter  margin-top-4 ">
+    <div className="containerCenter  margin-top-4 "   ref={scrollEl}>
 
         <Title title="4 Aşamada Araba Kirala" p="  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua"></Title>
@@ -20,27 +78,27 @@ function Info() {
     <div className="container row around-xs continer-60 margin-top-4">
 
     <InfoCard title="Arama Yap" p="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed"
-    svg={search} color={true}
+    Svg={<Search  color={level==1}></Search >} color={level==1}
     />
-    <Wave color={true}></Wave>
+    <Wave color={level==1}></Wave>
 
     <InfoCard title="En uygununu Bul" p="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed"
-    svg={photoIcon} color={false}
+    Svg={<Image  color={level==2}></Image >} color={level==2}
     />
-    <Wave></Wave>
+    <Wave color={level==2}></Wave>
     
     <InfoCard title="Arabanı Seç" p="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed"
-    svg={carIcon} color={false}
+  Svg={<Car  color={level==3}></Car >} color={level==3}
     />
-    <Wave></Wave>
+   <Wave color={level==3}></Wave>
  
     <InfoCard title="Ödemeni Yap" p="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed"
-    svg={paymentIcon} color={false}
+    Svg={<Card  color={level==4}></Card >}  color={level==4}
     />
- <Wave></Wave>
+ <Wave color={level==4}></Wave>
  
  <InfoCard title="Araban Sende !" p="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed"
- svg={keywordIcon} color={false}
+ Svg={<Key  color={level==5}></Key >} color={level==5}
  />
     </div>
  
