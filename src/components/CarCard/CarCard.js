dd62c575e41e5f96337559ca12e6ca5f15099315
orkunmertgs98 <orkunmertgs41@gmail.com>
2020-09-25 22:36:23 +0300

    s

--- src/components/CarCard/CarCard.js
+++ src/components/CarCard/CarCard.js
@@ -1,13 +1,77 @@
-import React from "react";
+import React,{useEffect,useState} from "react";
 import "./CarCard.style.css";
-import fuel from "../../assets/svg/fuel.svg";
-import direksiyon from "../../assets/svg/direksiyon.svg";
-import ondenaraba from "../../assets/svg/ondenaraba.svg";
-import vites from "../../assets/svg/vites.svg";
+import Fuel from "./fuel";
+import Direksiyon from "./direksiyon";
+import Ondenaraba from "./ondenaraba";
+import Vites from "./vites";
 
 function CarCard({ name, driverAge, licenceYear, price, image }) {
+
+
+  const [enable,set_enable]=useState(false)
+
+  const [level,set_level]=useState(0)
+  
+  
+    let scrollEl = React.createRef();
+   const  handleScroll = e => {
+  
+      let element = scrollEl.current
+     if(!enable&&element){
+      if (window.pageYOffset>element.scrollHeight ) {
+        // do something at end of scroll
+        console.log(" 1 kez")
+        setTimeout(() => {
+          set_enable(true)
+        }, 1000);
+       
+      }
+  
+     }
+    
+    }
+  
+    useEffect(() => {
+  
+     
+  
+      window.addEventListener("scroll", handleScroll);
+      return () => {
+        window.removeEventListener("scroll", handleScroll);
+      };
+    });
+  
+  const increaseLevel=(l)=>{
+  
+  setTimeout(() => {
+    set_level(l)
+    if(l<5)
+    increaseLevel(l+1)
+    else set_level(0)
+  }, 1000);
+  
+  }
+  
+  
+  
+    useEffect(() => {
+  if(enable){
+  
+  set_level(1)
+  increaseLevel(2)
+  
+  
+  
+  }
+     
+  
+    },[enable]);
+  
+
+
+
   return (
-    <div className="col-lg-3 col-md-4 col-xs-12 margin-top-1 margin-bottom-1">
+    <div className="col-lg-3 col-md-4 col-xs-12 margin-top-1 margin-bottom-1" >
       <div className="car-card shadow box">
         <img src={image} />
         <div className="car-card-details">
@@ -23,19 +87,32 @@ function CarCard({ name, driverAge, licenceYear, price, image }) {
             </p>
           </div>
 
-          <div className="car-card-options">
-            <div className="car-card-option fuel ">
-           
-              <img src={fuel} />
+          <div className="car-card-options  " ref={scrollEl}>
+            <div className={`${level==1?"back-lacivert car-card-option":"car-card-option"}`} >
+              <div className={`${level==1?"hover":"popup-modal"}`}>
+                Dizel
+           </div>
+              <Fuel enable={level==1}></Fuel>
             </div>
-            <div className="car-card-option">
-              <img src={vites} />
+            <div className={`${level==2?"back-lacivert car-card-option":"car-card-option"}`}>
+              <div className={`${level==2?"hover":"popup-modal"}`}>
+                Dizel
+           </div>
+              <Vites  enable={level==2}>
+              </Vites>
             </div>
-            <div className="car-card-option">
-              <img src={ondenaraba} />
+            <div className={`${level==3?"back-lacivert car-card-option":"car-card-option"}`}>
+              <div className={`${level==3?"hover":"popup-modal"}`}>
+                Dizel
+           </div>
+              <Ondenaraba  enable={level==3}/>
             </div>
-            <div className="car-card-option">
-              <img src={direksiyon} />
+            <div className={`${level==4?"back-lacivert car-card-option":"car-card-option"}`}>
+              <div className={`${level==4?"hover":"popup-modal"}`}>
+                Dizel
+           </div>
+
+            <Direksiyon  enable={level==4}/>
             </div>
           </div>
 
@@ -44,7 +121,7 @@ function CarCard({ name, driverAge, licenceYear, price, image }) {
               <p>Günlük Fiyat</p>
               <strong>{price}₺</strong>
             </div>
-            <button className="background-white">Hemen Kirala</button>
+            <button className="background-white"  >Hemen Kirala</button>
           </div>
         </div>
       </div>
---
+++ src/components/CarCard/fuel.js
@@ -0,0 +1,16 @@
+import React from 'react'
+//fill={`${enable?"#fff":"#023047"}`}
+export default function fuel({enable}) {
+    return (
+        <div>
+            <svg  id="XMLID_804_" xmlns="http://www.w3.org/2000/svg" width="14.762" height="19.304" viewBox="0 0 14.762 19.304">
+  <path id="XMLID_805_" d="M60,60v1.136h1.136v2.271H73.626V61.136h1.136V60Z" transform="translate(-60 -57.729)"  className={`${enable?"active":"notactive"}`}/>
+  <path id="XMLID_806_" d="M138.407,1.136V0H135V1.136Z" transform="translate(-132.161)"   className={`${enable?"active":"notactive"}`}/>
+  <path id="XMLID_807_" d="M225,253.79a1.136,1.136,0,1,0,2.271,0,4.493,4.493,0,0,0-1.136-1.818A4.494,4.494,0,0,0,225,253.79Z" transform="translate(-218.755 -242.435)"   className={`${enable?"active":"notactive"}`}/>
+  <path id="XMLID_808_" d="M73.626,420H61.136v2.271H60v1.135H74.762v-1.135H73.626Z" transform="translate(-60 -404.103)"   className={`${enable?"active":"notactive"}`}/>
+  <path id="XMLID_809_" d="M102.491,180H90v7.949h12.491Zm-6.245,6.813a2.274,2.274,0,0,1-2.271-2.271,3.657,3.657,0,0,1,.972-2.027,12.576,12.576,0,0,1,.9-1.023l.4-.4.4.4a12.585,12.585,0,0,1,.9,1.023,3.657,3.657,0,0,1,.972,2.027A2.274,2.274,0,0,1,96.245,186.813Z" transform="translate(-88.864 -173.187)"   className={`${enable?"active":"notactive"}`}/>
+</svg>
+
+        </div>
+    )
+}
