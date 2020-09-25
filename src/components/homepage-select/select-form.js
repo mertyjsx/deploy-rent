8ebdd8954869a8dbf3266869fad1fd614ba9c8f0
unknown <enesbc46@gmail.com>
2020-09-25 23:07:19 +0300

    d

--- src/components/homepage-select/select-form.js
+++ src/components/homepage-select/select-form.js
@@ -1,87 +1,90 @@
-import React from 'react'
-import DatePicker from "react-datepicker"
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import ArrowRight from "../../assets/svg/arrow-right.svg"
+import ArrowRight from "../../assets/svg/arrow-right.svg";
 export default function SelectForm() {
-    return (
-        <div>
-
-<select className="select-input">
-  <option value="grapefruit">Teslim alınacak konum</option>
-  <option value="lime">Lime</option>
-  <option value="coconut">Coconut</option>
-  <option value="mango">Mango</option>
-</select>
-            
-        <div className="row center-xs between-xs margin-bottom-1">
-     
-<div className="col-xs-7 start-xs ">
-<DatePicker
-    className="date-input "
-      isClearable
-      placeholderText="Alış tarihi"
-    />
-
-</div>
-
-<div className="col-xs-4  end-xs">
-<DatePicker
-    className="time-input "
-    showTimeSelect
-    showTimeSelectOnly
-    timeIntervals={15}
-    timeCaption="Time"
-    dateFormat="h:mm aa"
-    placeholderText="Saat"
-    />
-
-</div>
-
-
+  const [checked, setChecked] = useState(false);
+  const handleChange = (e) => {
+    setChecked(e.target.checked);
+  };
+  return (
+    <div>
+      <select className={checked ? "checked-input" : "select-input"}>
+        <option value="grapefruit">Teslim alınacak konum</option>
+        <option value="lime">Lime</option>
+        <option value="coconut">Coconut</option>
+        <option value="mango">Mango</option>
+      </select>
+      {checked ? (
+        <select className={checked ? "checked-input" : "select-input"}>
+          <option value="grapefruit">Teslim edilecek konum</option>
+          <option value="lime">Lime</option>
+          <option value="coconut">Coconut</option>
+          <option value="mango">Mango</option>
+        </select>
+      ) : null}
+      <div className="row center-xs between-xs margin-bottom-1">
+        <div className="col-xs-7 start-xs ">
+          <DatePicker
+            className="date-input "
+            isClearable
+            placeholderText="Alış tarihi"
+          />
         </div>
 
-        <div className="row center-xs between-xs ">
-     
-     <div className="col-xs-7 start-xs ">
-     <DatePicker
-         className="date-input "
-           isClearable
-           placeholderText="Alış tarihi"
-         />
-     
-     </div>
-     
-     <div className="col-xs-4 end-xs">
-     <DatePicker
-     
-         className="time-input  "
-         showTimeSelect
-         showTimeSelectOnly
-         timeIntervals={15}
-         timeCaption="Time"
-         dateFormat="h:mm aa"
-         placeholderText="Saat"
-         />
-     
-     </div>
-     
-     
-             </div>
-            
-<div className="row between-xs margin-top-2 padding-left-1">
-
-<div className="row center-xs  ">
-<input type="checkbox" className="margin-right-1 " />
-<p>Aracı farklı yerde teslim edeceğim.</p>
-</div>
-<div className="rent-button center-xs row xs-margin-top-1">
-Arabanı Seç
-<img  className="margin-left-1 center-xs padding-top-half" src={ArrowRight}></img>
-
-</div>
-</div>
-
-
+        <div className="col-xs-4  end-xs">
+          <DatePicker
+            className="time-input "
+            showTimeSelect
+            showTimeSelectOnly
+            timeIntervals={15}
+            timeCaption="Time"
+            dateFormat="h:mm aa"
+            placeholderText="Saat"
+          />
         </div>
-    )
+      </div>
+
+      <div className="row center-xs between-xs ">
+        <div className="col-xs-7 start-xs ">
+          <DatePicker
+            className="date-input "
+            isClearable
+            placeholderText="Alış tarihi"
+          />
+        </div>
+
+        <div className="col-xs-4 end-xs">
+          <DatePicker
+            className="time-input  "
+            showTimeSelect
+            showTimeSelectOnly
+            timeIntervals={15}
+            timeCaption="Time"
+            dateFormat="h:mm aa"
+            placeholderText="Saat"
+          />
+        </div>
+      </div>
+
+      <div className="row between-xs margin-top-2 padding-left-1">
+        <div className="row center-xs  ">
+          <input
+            type="checkbox"
+            className="margin-right-1 "
+            onChange={handleChange}
+          />
+          <p>Aracı farklı yerde teslim edeceğim.</p>
+        </div>
+        <div className="rent-button center-xs row xs-margin-top-1">
+          Arabanı Seç
+          <img
+            className="margin-left-1 center-xs padding-top-half"
+            src={ArrowRight}
+            alt=""
+          ></img>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/navbar/navbarBottom.js
+++ src/components/navbar/navbarBottom.js
@@ -1,29 +1,66 @@
-import React from 'react'
-import "./navbarBottom.css"
-import Logo from "../../assets/svg/navbarLogo.svg"
-import Ring from "../../assets/svg/ring.svg"
-import Gift from "../../assets/svg/gift.svg"
-export default function navbarBottom() {
-    return (
-        <div>
-            <div className="navbarBottomContainer  center-xs visibleAfterMd">
-            <div className="container container-60 center-xs between-xs row h-100">
-<ul className="col-md-10 col-lg-10 row">
-    <img src={Logo} className="logo-position"></img>
-<li className="center-xs box-large">Kiralama Kampanyaları</li>
-<li className="center-xs box-large font-bold">Araçlarımız</li>
-<li className="center-xs box-large">Filo</li>
-<li className="center-xs box-large">İletişim</li>
-<li className="center-xs box-large">Ofisler</li>
+import React, { useState } from "react";
+import "./navbarBottom.css";
+import Logo from "../../assets/svg/navbarLogo.svg";
+import Ring from "../../assets/svg/ring.svg";
+import Gift from "../../assets/svg/gift.svg";
+import CartDropdown from "../CartDropDown/CartDropDown.jsx";
 
-</ul>
-<div className=" center-xs">
-<img src={Gift} className="logo-position"></img>
-<img src={Ring} ></img>
+const NavbarBottom = () => {
+  const [hidden, setHidden] = useState(true);
+  const setActiveLink = (e) => {
+    // easier for me, you can change with getElementById or getElementByClassName
+    const links = document.getElementsByTagName("li");
 
-</div>
-            </div>
-            </div>
+    Array.from(links).forEach((el) => el.classList.remove("active"));
+    e.target.classList.add("active");
+  };
+  const toogleHidden = () => {
+    setHidden(!hidden);
+  };
+  return (
+    <div>
+      <div className="navbarBottomContainer  center-xs visibleAfterMd">
+        <div className="container container-60 center-xs between-xs row h-100">
+          <ul className="col-md-10 col-lg-10 row">
+            <img src={Logo} className="logo-position" alt=""></img>
+            <li className="center-xs box-large" onClick={setActiveLink}>
+              Kiralama Kampanyaları
+            </li>
+            <li
+              className="center-xs box-large font-bold"
+              onClick={setActiveLink}
+            >
+              Araçlarımız
+            </li>
+            <li className="center-xs box-large" onClick={setActiveLink}>
+              Filo
+            </li>
+            <li className="center-xs box-large" onClick={setActiveLink}>
+              İletişim
+            </li>
+            <li className="center-xs box-large" onClick={setActiveLink}>
+              Ofisler
+            </li>
+          </ul>
+          <div className=" center-xs">
+            <img
+              src={Gift}
+              className="logo-position"
+              alt=""
+              style={{ cursor: "pointer" }}
+              onClick={toogleHidden}
+            ></img>
+            <img
+              src={Ring}
+              alt=""
+              style={{ cursor: "pointer" }}
+              onClick={toogleHidden}
+            ></img>
+          </div>
         </div>
-    )
-}
+      </div>
+      {hidden ? null : <CartDropdown />}
+    </div>
+  );
+};
+export default NavbarBottom;
